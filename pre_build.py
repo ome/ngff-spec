10782d750cffdb27f46178da3ddf712e1c4a4b2e
Johannes Soltwedel <38459088+jo-mueller@users.noreply.github.com>
2026-09-14 12:49:55 +0200

    Prepend anchors 0.2 (#209)
    * feat: prepend anchors with version key
    
    * docs: do not include index.md for children pages
    
    * docs: prepend anchors with hyphens

--- pre_build.py
+++ pre_build.py
@@ -4,6 +4,7 @@ import glob
 from pathlib import Path
 import jsonc as json
 import logging
+from _version import __version__
 
 # Suppress warnings from json-schema-for-humans about unresolvable URLs
 logging.getLogger().setLevel(logging.ERROR)
@@ -51,7 +52,7 @@ This document contains JSON examples for {category} metadata layouts.
             for json_file in json_files:
                 print(f'Processing {json_file}...')
 
-                crossref = f"examples:{category}:{Path(json_file).stem}"
+                crossref = f"{__version__}-examples-{category}-{Path(json_file).stem}"
                 index_md += f"- [{Path(json_file).stem}](#{crossref})\n"
 
                 json_file_name = Path(json_file).stem
@@ -118,7 +119,7 @@ Find below links to auto-generated markdown pages or interactive HTML pages for 
             # insert mySt cross-reference at top of markdown files
             with open(output_path_md, 'r', encoding='utf-8') as md_file:
                 md_content = md_file.read()
-            crossref = f"schemas:{Path(schema_file).stem}"
+            crossref = f"{__version__}-schemas-{Path(schema_file).stem}"
             md_content = f"""---
 author: ""
 ---
