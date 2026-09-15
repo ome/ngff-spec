584f56f20273205827186b836499fb4344e22e7d
Chris Barnes <chris.barnes@gerbi-gmb.de>
2026-09-15 15:44:04 +0100

    Prepend version to anchors
    Also use hyphens rather than colons.
    
    https://github.com/ome/ngff-spec/pull/205
    
    Co-Authored-By: Johannes Soltwedel <38459088+jo-mueller@users.noreply.github.com>

--- pre_build.py
+++ pre_build.py
@@ -6,6 +6,8 @@ from pathlib import Path
 
 import jsonc as json
 
+from _version import __version__
+
 # Suppress warnings from json-schema-for-humans about unresolvable URLs
 logging.getLogger().setLevel(logging.ERROR)
 
@@ -47,7 +49,7 @@ This document contains JSON examples for {example} metadata layouts.
         for json_file in json_files:
             print(f'Processing {json_file}...')
 
-            crossref = f"examples:{example}:{Path(json_file).stem}"
+            crossref = f"{__version__}-examples-{example}-{Path(json_file).stem}"
             index_md += f"- [{Path(json_file).stem}](#{crossref})\n"
 
             json_file_name = Path(json_file).stem
@@ -114,7 +116,7 @@ Find below links to auto-generated markdown pages or interactive HTML pages for 
             # insert mySt cross-reference at top of markdown files
             with open(output_path_md, 'r', encoding='utf-8') as md_file:
                 md_content = md_file.read()
-            crossref = f"schemas:{Path(schema_file).stem}"
+            crossref = f"{__version__}-schemas-{Path(schema_file).stem}"
             md_content = f"""---
 author: ""
 ---
